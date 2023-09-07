a28b3b1a51c1eecb25e0fc3f73732a4bce95d53e
samueljsong <semuall0108@gmail.com>
2023-09-07 12:47:35 -0700

    pushing before making server branch

--- databaseConnection.js
+++ databaseConnection.js
@@ -1,15 +1,15 @@
 require('dotenv').config();
 const mysql = require('mysql2/promise');
-
-console.log(process.env.MYSQL_HOST);
 // Connecting to MySQL
 let dbConfig = mysql.createConnection({
-    host: process.env.MYSQL_HOST,
+    host: process.env.MYSQL_HOST,   
     port: process.env.MYSQL_PORT,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASS,
     database: process.env.MYSQL_NAME
 })
+
+console.log(dbConfig);
 
 let database = mysql.createPool(dbConfig);
 
