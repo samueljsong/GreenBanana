3d09b580e68c5a8e1fb53d18f87f6bc55df9d61e
samueljsong <semuall0108@gmail.com>
2023-09-07 08:21:14 -0700

    fixed merge conflicts

--- server/server.js
+++ server/server.js
@@ -34,21 +34,6 @@ let mongoStore = MongoStore.create({
     collectionName: "sessions"
 })
 
-// Connecting to MySQL
-// let con = mysql.createConnection({
-//     host: process.env.MYSQL_HOST,
-//     port: process.env.MYSQL_PORT,
-//     user: process.env.MYSQL_USER,
-//     password: process.env.MYSQL_PASS,
-//     database: process.env.MYSQL_NAME,
-//     multipleStatements: false,
-//     namedPlaceholders: true
-// })
-
-// con.connect( (err) => {
-//     if(err) throw err;
-//     console.log('MySQL: connected...')
-// })
 
 // Connect to Cloudinary
 cloudinary.config({
